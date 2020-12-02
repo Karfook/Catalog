aee3dd31aa4a7cf388b8ab36330b3f4b1558b2ea
Karfook <72116522+Karfook@users.noreply.github.com>
2020-12-02 19:02:32 +0800

    Update results.php

--- results.php
+++ results.php
@@ -9,25 +9,69 @@
     <h1>Book Search Results</h1>
     <?php
     // TODO 1: Create short variable names.
+        $searchtype = '';
+        $searchterm = '';
 
+        $isbn = '';
+        $author = '';
+        $title = '';
+        $price = '';
 
     // TODO 2: Check and filter data coming from the user.
-
+        if(isset($_POST['searchtype']) && isset($_POST['searchterm']))
+        {
+            $searchtype = htmlspecialchars($_POST['searchtype']);
+            $searchterm = htmlspecialchars($_POST['searchterm']);
+        }
 
     // TODO 3: Setup a connection to the appropriate database.
-
+        include('config.php');
 
     // TODO 4: Query the database.
+        $sql = "SELECT * FROM catalogs WHERE " . $searchtype . " LIKE '%$searchterm%'";
 
+        $result = $conn -> query($sql);
+
+        if(!$result)
+            die ("Fail to retrieve data: " . $conn -> error);
 
     // TODO 5: Retrieve the results.
+        // TODO 6: Display the results back to user.
+        if($result -> num_rows>0)
+        {
+            $number = 1;
+            ?>
+                <table border=1>
+                    <tr>
+                        <th>No.</th>
+                        <th>ISBN</th>
+                        <th>Author</th>
+                        <th>Title</th>
+                        <th>Price</th>
+                    </tr>
+            <?php
+            while($row = $result -> fetch_assoc())
+            {
+                $isbn = $row['isbn'];
+                $author = $row['author'];
+                $title = $row['title'];
+                $price = $row['price'];
 
+                // $number = $number++;
 
-    // TODO 6: Display the results back to user.
-
-
+                ?>
+                    <tr>
+                        <td><?php echo $number++; ?></td>
+                        <td><?php echo $isbn; ?></td>
+                        <td><?php echo $author; ?></td>
+                        <td><?php echo $title; ?></td>
+                        <td><?php echo $price; ?></td>
+                    </tr>
+                <?php
+            }
+        }
     // TODO 7: Disconnecting from the database.
-
+        $conn -> close();
 
     ?>
 </body>
